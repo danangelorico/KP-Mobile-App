dd933b77f8c916a2332f38acb4b1638ed11bd3e9
MotivIT Applications <DevTeam@SOLUTIONaaSs-Mac-mini.local>
2019-08-09 17:07:08 +0800

    Mobile App Part 1

---
+++ Kaiser Permanente Mobile App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kaiser Permanente Mobile App
+//
+//  Created by MotivIT Applications on 8/9/19.
+//  Copyright © 2019 MotivIT Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
